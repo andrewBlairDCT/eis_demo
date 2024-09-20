0b0cda53fbfb6f87f4e9cb8182127796b7d77c04
AndrewBlairDCT <andrew.blair@dcthomson.co.uk>
2024-09-20 10:58:59 +0100

    filters out non image files

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import { Fragment, useState } from "react";
+import { Fragment, useState, useRef } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const Upload = () => {
   const [files, setFiles] = useState<File[]>([]);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const allowedFileTypes = ["image/png", "image/jpeg", "image/jpg"];
 
   const Bucket = process.env.NEXT_PUBLIC_AWS_BUCKET_NAME;
   const s3 = new S3Client({
@@ -19,13 +21,26 @@ const Upload = () => {
   const handleUploadLocalFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     if (!e.target.files) return;
+
     const selectedFiles = Array.from(e.target.files);
-    setFiles(selectedFiles);
+    const filteredFiles = selectedFiles.filter((file) =>
+    allowedFileTypes.includes(file.type));
+
+    if (filteredFiles.length !== selectedFiles.length) 
+    { 
+      alert('Some files are not the required image types. Please choose files again.');
+      if (fileInputRef.current) {
+        fileInputRef.current.value = "";
+      }
+    }
+
+    setFiles(filteredFiles);
   };
 
    const handleUploadS3 = async (e: any) => {
+    e.preventDefault();
      if (files.length === 0) {
-      alert('Please select a file to upload.')
+      alert('Please select file(s) to upload.')
       return
     }
     try {
@@ -51,20 +66,20 @@ const Upload = () => {
   };
 
 
-
   return (
     <Fragment>
       <div>
         <div>
           <div>
+            <p>Acceptable file formats: jpg, jpeg, png</p>
             <div>
               <input
                 type="file"
                 id="image-upload"
-               
                 accept=".png, .jpg, .jpeg"
                 onChange={handleUploadLocalFile}
                 multiple
+                ref={fileInputRef}
               />
             </div>
           </div>
